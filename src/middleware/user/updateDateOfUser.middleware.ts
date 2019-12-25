9faefb0be472155ddd346b5854431b9dddd194a1
Vitaliy Demchyshyn <demchyshyn.v87@gmail.com>
2019-12-25 14:46:29 +0200

    fixed some bugs of validator

--- src/middleware/user/updateDateOfUser.middleware.ts
+++ src/middleware/user/updateDateOfUser.middleware.ts
@@ -1,15 +1,15 @@
-import {NextFunction, Response} from "express";
+import {NextFunction, Response} from 'express';
 
-import {IRequestExtended, IUser} from "../../interfaces";
-import {userService} from "../../services/user";
+import {IRequestExtended, IUser} from '../../interfaces';
+import {userService} from '../../services/user';
 
 export const updateDate = async (req: IRequestExtended, res: Response, next: NextFunction) => {
     try {
         const user = req.user as IUser;
         user.updated_at = Date.now().toString();
         await userService.updateUser(user._id, user);
-        next()
+        next();
     } catch (e) {
-        next(e)
+        next(e);
     }
 };

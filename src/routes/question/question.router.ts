512778d5d11ceab7088b1f890f38b18f54c80fe6
Oleh Kosylo <kosylopoker@gmail.com>
2020-07-15 13:00:44 +0300

    [BE] LMS-170
    Fixed mistakes about deleting question for admin

--- src/routes/question/question.router.ts
+++ src/routes/question/question.router.ts
@@ -5,7 +5,7 @@ import { questionController } from '../../controllers';
 import {
   checkAccessTokenMiddleware, checkIsAdmin, isQuestionFilterValid,
   isQuestionPresentMiddleware, isQuestionValid,
-  isUserAdminOrTeacherMiddleware, isUserQuestionOwnerMiddleware
+  isUserAdminOrTeacherMiddleware
 } from '../../middleware';
 
 const router = Router();
@@ -17,7 +17,7 @@ router.use(checkIsAdmin);
 router.get('/my', questionController.getMyQuestions);
 router.post('/', isQuestionValid, isUserAdminOrTeacherMiddleware, questionController.createQuestion);
 
-router.use('/:question_id', isQuestionPresentMiddleware, isUserQuestionOwnerMiddleware);
+router.use('/:question_id', isQuestionPresentMiddleware);
 router.delete('/:question_id', questionController.deleteQuestion);
 
 export const questionRouter = router;

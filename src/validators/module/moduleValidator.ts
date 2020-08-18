e9eeb6be673298e8c2b4091e34f72dccd47f5b56
Oleh Kosylo <kosylopoker@gmail.com>
2020-08-18 15:38:32 +0300

    [BE][ADMIN] LMS-196 Add functional for delete and add lesson of module

--- src/validators/module/moduleValidator.ts
+++ src/validators/module/moduleValidator.ts
@@ -3,7 +3,7 @@ import * as Joi from 'joi';
 export const moduleValidator = Joi.object().keys({
   label: Joi.string().max(255).trim().required(),
   description: Joi.string().max(255).trim(),
-  tag_list: Joi.array().items(Joi.string()),
+  tags: Joi.array().items(Joi.string()),
   courses_id: Joi.array().items(Joi.string()),
   lessons_list: Joi.array().items(Joi.string())
 });
